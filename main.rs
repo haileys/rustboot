468bc8d8c91cdf9b8666e6cd06a0dae86a296153
YangFan789 <yangfan789@outlook.com>
2017-02-21 14:17:54 +0800

    Fix main.rs for rustc 1.17.0-nightly (0e7727795 2017-02-19)

--- main.rs
+++ main.rs
@@ -1,52 +1,30 @@
+#![feature(custom_attribute)]
 #![no_std]
-#![allow(ctypes)]
+#![allow(dead_code)]
+#![allow(unused_attributes)]
 
+#[derive(Copy, Clone)]
 enum Color {
-    Black      = 0,
-    Blue       = 1,
-    Green      = 2,
-    Cyan       = 3,
-    Red        = 4,
-    Pink       = 5,
-    Brown      = 6,
-    LightGray  = 7,
-    DarkGray   = 8,
-    LightBlue  = 9,
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Pink = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
     LightGreen = 10,
-    LightCyan  = 11,
-    LightRed   = 12,
-    LightPink  = 13,
-    Yellow     = 14,
-    White      = 15,
-}
-
-enum Option<T> {
-    None,
-    Some(T)
-}
-
-struct IntRange {
-    cur: int,
-    max: int
-}
-
-impl IntRange {
-    fn next(&mut self) -> Option<int> {
-        if self.cur < self.max {
-            self.cur += 1;
-            Some(self.cur - 1)
-        } else {
-            None
-        }
-    }
-}
-
-fn range(lo: int, hi: int) -> IntRange {
-    IntRange { cur: lo, max: hi }
+    LightCyan = 11,
+    LightRed = 12,
+    LightPink = 13,
+    Yellow = 14,
+    White = 15,
 }
 
 fn clear_screen(background: Color) {
-    for i in range(0, 80 * 25) {
+    for i in 0..80 * 25 {
         unsafe {
             *((0xb8000 + i * 2) as *mut u16) = (background as u16) << 12;
         }
@@ -56,5 +34,5 @@ fn clear_screen(background: Color) {
 #[no_mangle]
 #[no_split_stack]
 pub fn main() {
-    clear_screen(LightRed);
+    clear_screen(Color::Cyan);
 }
